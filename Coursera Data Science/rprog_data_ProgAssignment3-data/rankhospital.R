165b7cb02a3fcb780dcb29b37d9a987ddfdb06df
KaiAragaki <adamaragaki@gmail.com>
2018-12-26 17:01:18 -0500

    Third problem done

---
+++ Coursera Data Science/rprog_data_ProgAssignment3-data/rankhospital.R
@@ -0,0 +1,40 @@
+rankhospital <- function(state, outcome, num = "best"){
+  data <- read.csv("outcome-of-care-measures.csv", na.strings = "Not Available") # Read in
+  data <- data[,c(2,7,11,17,23)] # Subset to only state, name and three condition mortalities
+  condition <- grep(paste0("^",outcome,"$"), c("heart attack", "heart failure", "pneumonia"))
+  if(sum(grepl(paste0("^",state,"$"), data[,2]))==0){
+    stop("invalid state")
+  }
+  else if (length(condition)==0){
+    stop("invalid outcome")
+  }
+  else{
+    stateData <- data[state == data[,2],]
+    stateCondition <- 
+      if(condition == 1) {
+        stateData[,-c(4:5)]
+      } 
+    else if (condition == 2) {
+      stateData[,-c(3,5)]
+    } 
+    else {
+      stateData[,-c(3:4)]
+    }
+    stateCondition <- stateCondition[complete.cases(stateCondition),]
+    stateCondition <- stateCondition[order(stateCondition[,3], stateCondition[,1]),]
+    ranks <- 1:nrow(stateCondition)
+    stateCondition <- cbind(stateCondition, ranks)
+    if (num == "best") {
+      hospital <- stateCondition[1,1]
+    }
+    else if (num == "worst") {
+      hospital <- stateCondition[nrow(stateCondition),1]
+    }
+    else {
+      hospital <- stateCondition[num,1]
+    }
+  }
+  hospital
+}
+
+
